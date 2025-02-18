7f3e083745b33e61b8a76348943237f85a765c9f
YahiaHabibb <yahiahabibtech@gmail.com>
2025-02-19 01:54:35 +0200

    03-AuthAcount

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,63 @@
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { signIn } from '@/auth'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Facebook, Github } from 'lucide-react'
 import React from 'react'
+import { FcGoogle } from 'react-icons/fc'
 
 const Loginpage = () => {
     return (
     <div className='flex flex-col items-center justify-center py-5 md:py-10 gap-5'>
         <div className='bg-gray-100 flex items-center justify-center'>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Sign in</CardTitle>
+            <Card className='w-full max-w-md'>
+                <CardHeader className='space-y-1'>
+                    <CardTitle className='text-2xl font-bold text-center'>Sign in</CardTitle>
+                        <CardDescription className='text-center'>
+                            Choose your preferred sign in method
+                        </CardDescription>
                 </CardHeader>
+                <CardContent className='space-y-4 w-full min-w-96'>
+                    <div className='space-y-2'>
+                        <form
+                            action={ async () => {
+                            "use server"
+                            await signIn("google")
+                            }}
+                            
+                        >
+                            <Button type='submit' variant='outline'
+                                className='w-full justify-center space-x-2 bg-white hover:bg-gray-100 hoverEffect'>
+                                <FcGoogle/>
+                                <span>Sign in with Google</span>
+                            </Button>
+                        </form>
+                        <form
+                            action={ async () => {
+                            "use server"
+                            await signIn("github")
+                            }}
+                        >
+                            <Button type='submit' variant='outline'
+                            className='w-full justify-center space-x-2 bg-white hover:bg-gray-100 hoverEffect'>
+                                <Github/>
+                                <span>Sign in with Github</span>
+                            </Button>
+                        </form>
+                        <form
+                            action={ async () => {
+                            "use server"
+                            await signIn("Facebook")
+                            }}
+                            
+                        >
+                            <Button type='submit' variant='outline'
+                            className='w-full justify-center space-x-2 bg-white hover:bg-gray-100 hoverEffect'>
+                                <Facebook className='w-5 h-5'/>
+                                <span>Sign in with Facebook</span>
+                            </Button>
+                        </form>
+                    </div>    
+                </CardContent>
             </Card>
         </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,23 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: "Switzer, system-ui, sans-serif",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        amazonBlue: "#131921",
+        amazonLight: "#232F3E",
+        lightText: "#ccc",
+        quantity_box: "#F0F2F2",
+        footerBottom: "#131A22",
+        amazonYellow: "#fcd814",
+        amazonYellowDark: "#f7ca00",
+        amazonOrange: "#fba41c",
+        amazonOrangeDark: "#fa8900",
+        amazonGreen: "#7fda69",
+        footerBg: "#232f3d",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
