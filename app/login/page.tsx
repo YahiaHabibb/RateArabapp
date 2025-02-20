5cd1af07ab08e24f12a311f1c17c9f09cca9762d
YahiaHabibb <yahiahabibtech@gmail.com>
2025-02-20 23:08:33 +0200

    authbig3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,16 @@
-import { signIn } from '@/auth'
+import { auth, signIn } from '@/auth'
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { Facebook, Github } from 'lucide-react'
+import { redirect } from 'next/navigation'
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
 
-const Loginpage = () => {
+const Loginpage = async () => {
+    const session = await auth();
+    if(session?.user) return redirect("/");
     return (
     <div className='flex flex-col items-center justify-center py-5 md:py-10 gap-5'>
         <div className='bg-gray-100 flex items-center justify-center'>
@@ -16,7 +21,7 @@ const Loginpage = () => {
                             Choose your preferred sign in method
                         </CardDescription>
                 </CardHeader>
-                <CardContent className='space-y-4 w-full min-w-96'>
+                <CardContent className='space-y-4 w-full min-w-[420] max-w-md'>
                     <div className='space-y-2'>
                         <form
                             action={ async () => {
@@ -56,8 +61,27 @@ const Loginpage = () => {
                                 <span>Sign in with Facebook</span>
                             </Button>
                         </form>
-                    </div>    
+                        <div className='relative'>
+                            <div className='absolute inset-0 flex item-center'>
+                                <span className='w-full border-t'></span>
+                            </div>
+                        </div>
+                        <div className='relative flex justify-center text-us uppercase'>
+                            <span className='bg-white px-2 text-gray-500'>or Continue With</span>
+                        </div>
+                    </div>
+                    <div className='space-y-2'>
+                        <Label htmlFor='email'>Email</Label>
+                        <Input id='email' type='email' placeholder='m@example.com' />
+                    </div>
+                    <div className='space-y-2'>
+                        <Label htmlFor='email'>Password</Label>
+                        <Input id='password' type='password' />
+                    </div>
                 </CardContent>
+                <CardFooter>
+                    <Button className='w-full bg-orange-500 hover:bg-orange-700 hoverEffect'>Sign in</Button>
+                </CardFooter>
             </Card>
         </div>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lhs.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
